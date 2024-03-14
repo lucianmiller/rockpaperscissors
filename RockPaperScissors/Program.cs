dca372fca66e02fc173e51ce2afa8234a1b67a70
Lucian Miller <lucian.miller185@gmail.com>
2024-03-14 11:16:04 -0700

    Add console app UI

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -7,7 +7,20 @@ namespace RockPaperScissors
   {
     static void Main()
     {
-      
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      Console.WriteLine("Welcome to rock, paper scissors!");
+      Console.WriteLine("Player 1, please enter your name: ");
+      string playerOne = Console.ReadLine();
+      Console.WriteLine("Player 2, please enter your name: ");
+      string playerTwo = Console.ReadLine();
+
+      Game newGame = new Game(playerOne, playerTwo);
+      Console.WriteLine(playerOne + " rock, paper or scissors?");
+      string playerOneInput = Console.ReadLine();
+      Console.WriteLine(playerTwo + " rock, paper or scissors?");
+      string playerTwoInput = Console.ReadLine();
+      Console.WriteLine(newGame.DetermineWinner(playerOneInput, playerTwoInput));
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
   }
 }
